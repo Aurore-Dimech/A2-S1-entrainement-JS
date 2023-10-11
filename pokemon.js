55ca9d6801177dd54faa29f5b51c2b1ae40afd01
Aurore-Dimech <aurore.dimech@edu.devinci.fr>
2023-10-11 12:06:26 +0200

    HTML + JS Poke + Taxi

--- pokemon.js
+++ pokemon.js
@@ -6,4 +6,44 @@ class Pokemon{
         this.hp = hp
         this.luck = luck
     }
+
+    isLucky() {
+        return (Math.random() < this.luck);
+    }
+    
+    attackPokemon(pokemon) {
+        let degatsRecus = this.attack - pokemon.defense
+        pokemon.hp -= degatsRecus
+        console.log(`Dégats reçus par ${pokemon.name}: ${degatsRecus} ; Vie restante : ${pokemon.hp}`)
+    }
 }
+
+let hoOh = new Pokemon('Ho-Oh', 100, 70, 106, 0.5)
+let raikou = new Pokemon('Raikou', 85, 75, 90, 0.5)
+
+while(hoOh.hp > 0 && raikou.hp > 0){
+    if(hoOh.isLucky()) {
+        hoOh.attackPokemon(raikou)
+        if (raikou.hp < 0)
+        {
+            console.log(`${raikou.name} est mort !`)
+            break;
+        }
+    } else {
+        console.log(`${raikou.name} a eu de la chance !`)
+    }
+
+    if(raikou.isLucky()) {
+        raikou.attackPokemon(hoOh)
+        if (hoOh.hp < 0)
+        {
+            console.log(`${hoOh.name} est mort !`)
+            break;
+        }
+    } else {
+        console.log(`${hoOh.name} a eu de la chance !`)
+    }
+
+    
+}
+
---
+++ taxi.js
@@ -0,0 +1,38 @@
+let personnage = {
+    name: 'John',
+    santeMentale: 10
+}
+
+let musiques = [
+    "Anissa de Wejdene",
+    "The Sun The Moon The Stars de Aether Realm",
+    "The Dancer and The Moon de Blackmore's Night",
+    "Into The Unknown de The Blasting Company",
+    "L'empire du côté obscure de IAM"
+]
+
+let trajet = {
+    radio : musiques[Math.floor(Math.random()*musiques.length)],
+    feuxRouges : 30,
+    changement : 0
+}
+
+while(trajet.feuxRouges > 0 && personnage.santeMentale > 0){
+        if(trajet.radio != musiques[0]){
+            trajet.feuxRouges-- ;
+            trajet.radio = musiques[Math.floor(Math.random()*musiques.length)]
+        } else {
+            personnage.santeMentale--
+            trajet.changement++
+            trajet.feuxRouges--
+            trajet.radio = musiques[Math.floor(Math.random()*musiques.length)]
+        }
+}
+
+if(personnage.santeMentale <= 0){
+    console.log('explosion')
+}
+
+if(trajet.feuxRouges == 0){
+    console.log(`John est bien arrivé à destination ! Cela lui aura demandé de changer de taxi ${trajet.changement} fois.`)
+}
